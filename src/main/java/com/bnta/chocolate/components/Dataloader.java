51eefcfdde99c682459268954399c2561aa813d1
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-05-17 15:19:23 +0100

    added data inputs

--- src/main/java/com/bnta/chocolate/components/Dataloader.java
+++ src/main/java/com/bnta/chocolate/components/Dataloader.java
@@ -1,4 +1,40 @@
 package com.bnta.chocolate.components;
 
-public class Dataloader {
+import com.bnta.chocolate.models.Chocolate;
+import com.bnta.chocolate.models.Estate;
+import com.bnta.chocolate.repositories.ChocolateRepository;
+import com.bnta.chocolate.repositories.EstateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dataloader implements ApplicationRunner {
+
+    @Autowired
+    ChocolateRepository chocolateRepository;
+
+    @Autowired
+    EstateRepository estateRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Estate estate1 = new Estate("Bob", "Australia");
+        estateRepository.save(estate1);
+
+        Estate estate2 = new Estate("Jill", "India");
+        estateRepository.save(estate2);
+
+        Chocolate choco1 = new Chocolate("Cadbury", 10, estate1);
+        chocolateRepository.save(choco1);
+
+        Chocolate choco2 = new Chocolate("Lint", 20, estate1);
+        chocolateRepository.save(choco2);
+
+        Chocolate choco3 = new Chocolate("Dove", 75, estate2);
+        chocolateRepository.save(choco3);
+
+
+    }
 }
